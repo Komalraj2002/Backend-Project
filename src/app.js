558312c378c3f61b34444feed865f73ef7c68041
komal raj singh <komalrajsingh2008@gmail.com>
2025-06-18 13:21:56 +0530

    comment controller

--- src/app.js
+++ src/app.js
@@ -11,22 +11,27 @@ app.use(
 );
 app.use(express.json({ limit: "16kb" })); //Parses JSON in request bodies from froentend AND the limit accepted depend on server power(bodyparser )
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); // parse the url in request body
-app.use(express.static("public"));  //public is a foler where we can store , serve static file from public folder
-app.use(cookieParser()) //	Parses cookies and puts them on req.cookies
+app.use(express.static("public")); //public is a foler where we can store , serve static file from public folder
+app.use(cookieParser()); //	Parses cookies and puts them on req.cookies
 
 //routes import
-import userRouter from "./routes/user.routes.js"
-import videoRouter from "./routes/video.routes.js"
-import tweetRouter from "./routes/tweet.routes.js"
-import subscriptionRouter  from "./routes/subscription.routes.js";
-import playlistRouter from "./routes/playlist.routes.js"
-import likeRouter from "./routes/like.routes.js"
-//routes declaration
-app.use("/api/v1/users" , userRouter)
-app.use("/api/v1/videos" , videoRouter )
-app.use("/api/v1/tweets" , tweetRouter)
-app.use("/api/v1/subscription" ,subscriptionRouter )
-app.use("/api/v1/playlist" ,playlistRouter )
-app.use("/api/v1/likes" ,likeRouter )
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routes.js";
+import tweetRouter from "./routes/tweet.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import likeRouter from "./routes/like.routes.js";
+import commentRouter from "./routes/comment.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
-export { app }; 
+//routes declaration
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscription", subscriptionRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/like", likeRouter);
+app.use("/api/v1/comment", commentRouter);
+app.use("/api/v1/comment", dashboardRouter);
+
+export { app };
---
+++ src/controllers/comment.controller.js
@@ -0,0 +1,154 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { apiError } from "../utils/apiError.js";
+import { apiResponse } from "../utils/apiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const addComment = asyncHandler(async (req, res) => {
+  // TODO: add a comment to a video
+  const { videoId } = req.params;
+  const { comment } = req.body;
+
+  if (!isValidObjectId(videoId)) {
+    throw new apiError(400, "invalid videoId");
+  }
+
+  if (!comment) {
+    throw new apiError(402, " comment not avilable");
+  }
+
+  const addComment = await Comment.create({
+    $set: {
+      content: comment,
+      video: videoId,
+      owner: req.user._id,
+    },
+  });
+
+  if (!addComment) {
+    throw new apiError(401, "comment cannot be added");
+  }
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, addComment, "comment added sucessfully"));
+});
+
+export const getVideoComments = asyncHandler(async (req, res) => {
+  const { videoId } = req.params;
+  let { page = 1, limit = 10 } = req.query;
+
+  if (!isValidObjectId(videoId)) {
+    throw new apiError(400, "videoId is invalid");
+  }
+
+  const allComment = await Comment.aggregate([
+    {
+      $match: {
+        video: mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $skip: (Number(page) - 1) * Number(limit),
+    },
+    {
+      $limit: Number(limit),
+    },
+    {
+      $project: {
+        content: 1,
+        owner: 1,
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: {
+          $project: {
+            username: 1,
+            avatar: 1,
+          },
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "likes",
+        localField: "_id",
+        foreignField: "comment",
+        as: "likes",
+      },
+    },
+    {
+      $addFields: {
+        likes: {
+          $size: "$likes",
+        },
+        owner: {
+          $first: "$owner",
+        },
+      },
+    },
+  ]);
+
+  if (!allComment) {
+    throw new apiError(402, "comment can not fetched");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new apiResponse(200, allComment, " all comments are fetched sucessfully")
+    );
+});
+
+export const updateComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
+  if (!isValidObjectId(commentId)) {
+    throw new apiError(400, " invalid commentId");
+  }
+
+  const updateComment = await Comment.findByIdAndUpdate(
+    commentId,
+    {
+      $set: {
+        content,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  if (!updateComment) {
+    throw new apiError(400, "comment not updated");
+  }
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, updateComment, "comment updated sucessfully"));
+});
+
+export const deleteComment = asyncHandler(async (req, res) => {
+  const { commentId } = req.params;
+
+  if (!isValidObjectId(commentId)) {
+    throw new apiError(400, " invalid commentId");
+  }
+
+  const deleteComment = await Comment.findByIdAndDelete(commentId);
+
+  if (!deleteComment) {
+    throw new apiError(400, "comment not deleted ");
+  }
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, deleteComment, "comment deleted sucessfully"));
+});
+
+export { addComment, getVideoComments, deleteComment, updateComment };
